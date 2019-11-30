bebe9185796da86879c458d8436682ff9113f133
HanbeulDean <hanbeul.dean.c@gmail.com>
2019-11-30 01:33:20 -0800

    DrawingOnHdc

--- WindowsProject1.cpp
+++ WindowsProject1.cpp
@@ -17,42 +17,51 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+struct _tagArea
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	bool bStart;
+	POINT ptStart;
+	POINT ptEnd;
+};
 
-    // TODO: 여기에 코드를 입력합니다.
+_tagArea g_tArea;
 
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WINDOWSPROJECT1, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// TODO: 여기에 코드를 입력합니다.
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
+	// 전역 문자열을 초기화합니다.
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_WINDOWSPROJECT1, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    MSG msg;
+	// 애플리케이션 초기화를 수행합니다:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
 
-    return (int) msg.wParam;
+	MSG msg;
+
+	// 기본 메시지 루프입니다:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
 }
 
 
@@ -64,23 +73,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	wcex.lpszMenuName = NULL;// MAKEINTRESOURCEW(IDC_WINDOWSPROJECT1);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -95,20 +104,20 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+	hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
 
 //
@@ -123,62 +132,141 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// 메뉴 선택을 구문 분석합니다:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
 
-			//유니코드 문자열은 "" 앞에 L을 붙여서 L""로 해줄 수 있다.
-			// 아니면 TEXT 매크로를 이용한다.
-			TextOut(hdc, 50, 50, L"win32", 5);
+	//마우스 왼쪽이 눌렀을 때 들어온다.
+	case WM_LBUTTONDOWN:
+		//마우승 위치는 lParam에 들어오게 되는데 16비트로 쪼개서 x,y값이
+		//32비트 변수에 들어오게 된다.
+		//LOWORD, HIWORD 매크로를 이용해서 하위, 상위 16비트의 값을 얻을 수 있다.
+		if (!g_tArea.bStart)
+		{
+			g_tArea.bStart = true;
+			g_tArea.ptStart.x = lParam & 0x0000ffff;
+			g_tArea.ptStart.y = lParam >> 16;
+			g_tArea.ptEnd = g_tArea.ptStart;
 
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+			//InvalidateRect 함수는 강제로 WM_PAINT 메시지를 호출해주는 함수이다.
+			//1번 인자는 윈도우 핸들이 들어간다.
+			//2번 인자는 초기화할 영역이 들어가는데
+			//NULL을 넣어줄 경우 전체 화면을 대상으로 갱신한다.
+			//3번 인자는 True일 경우 현재화면을 지우고 갱신
+			//false일 경우현재 화면을 안지우고 갱신.
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
+		break;
+
+		//마우스가 움직일때 들어오는 메시지이다.
+	case WM_MOUSEMOVE:
+		if (g_tArea.bStart) {
+			g_tArea.ptEnd.x = lParam & 0x0000ffff;
+			g_tArea.ptEnd.y = lParam >> 16;
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
+		break;
+
+	case WM_LBUTTONUP:
+		if (g_tArea.bStart)
+		{
+			g_tArea.bStart = false;
+			g_tArea.ptEnd.x = lParam & 0x0000ffff;
+			g_tArea.ptEnd.y = lParam >> 16;
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
+
+	//키가 눌렀을 때 들어오는 메시지.
+	case WM_KEYDOWN:
+		//이 메시지가 들어올 경우 wParam에 어떤 키가 눌렀는지가 들어온다.
+		switch (wParam) {
+		case VK_ESCAPE:
+			DestroyWindow(hWnd);
+			break;
+		}
+		break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+
+		//유니코드 문자열은 "" 앞에 L을 붙여서 L""로 해줄 수 있다.
+		// 아니면 TEXT("") 매크로를 이용한다. 내부적으로 유니코드와 멀티바이트 구분한다.
+		TextOut(hdc, 50, 50, TEXT("win3243"), 7);
+
+		//wsprintf : 유니코드 문자열을 만들어주는 함수이다.
+		// %d에는 정수가 대입된다.
+		TCHAR strMouse[64] = {};
+		wsprintf(strMouse, TEXT("x : %d y : %d"), g_tArea.ptStart.x,
+			g_tArea.ptStart.y);
+
+		//lstrlen : 유니코드 문자열의 길이를 구해주는 함수이다.
+		TextOut(hdc, 600, 30, strMouse, lstrlen(strMouse));
+
+		//사각형을 그리는 함수 Ractangle(,lefttop, rightbottom);
+		Rectangle(hdc, 100, 100, 200, 200);
+
+		//원형을 그리는 함수 매개변수는 Ractangle과 같음
+		Ellipse(hdc, 200, 100, 400, 200);
+
+		//선 그리기
+		MoveToEx(hdc, 300, 100, NULL);	//시작점 지정
+		LineTo(hdc, 400, 150);			//끝점 지정
+		LineTo(hdc, 500, 100);			//이전의 끝점이 시작점이 됨
+
+		MoveToEx(hdc, 100, 400, NULL);	//시작점 재지정
+		LineTo(hdc, 200, 500);			//끝점 지정
+
+		if (g_tArea.bStart)
+		{
+			Rectangle(hdc, g_tArea.ptStart.x, g_tArea.ptStart.y,
+				g_tArea.ptEnd.x, g_tArea.ptEnd.y);
+		}
+
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 // 정보 대화 상자의 메시지 처리기입니다.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
